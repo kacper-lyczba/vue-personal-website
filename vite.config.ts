89656b27bba31395a21e12c052abdd566ffbe061
kacper-lyczba <kacper.lyczba@gmail.com>
2023-07-20 17:03:58 +0200

    gh-pages requirements

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vue-personal-website/',
   plugins: [
     vue(),
     vueJsx(),
